21ca9702e393424a4dfa8a3cfa4ef7318fb68b41
22DP2RPete <131154830+22DP2RPete@users.noreply.github.com>
2024-02-27 07:54:03 +0000

    Papilditi darbini

--- src/main/java/rvt/DefaultController.java
+++ src/main/java/rvt/DefaultController.java
@@ -1,26 +1,46 @@
 package rvt;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import rvt.temp.A;
 
 @Controller
 public class DefaultController {
     
-    @GetMapping(value = "/")
-    ModelAndView index(@RequestParam(name="name", required=false, defaultValue="null") String name) {
-        Student student = new Student("John", "Smith", "a@a.lv", "DP2-4");
-        List<Student> students = new ArrayList<>();
-        students.add(student);
+//    @GetMapping(value = "/")
+//     String index(@RequestParam(name="name", required=false, defaultValue="null") String name, Model model) {
+//         return "index";
+//     }
 
-        ModelAndView modelAndView = new ModelAndView("index");
-        modelAndView.addObject("students", students);
-        modelAndView.addObject("date", new Date().toString());
-        return modelAndView;
-    }
+//     @GetMapping(value = "/test")
+//     ModelAndView testAction()
+//     {
+//         ModelAndView modelAndView = new ModelAndView("test");
+
+//         String name = "john";
+//         int age = 20;
+//         Money macins1 = new Money(21, 90);
+//         Money macins2 = new Money(30, 8);
+//         Money macins3 = new Money(23, 67);
+
+//         modelAndView.addObject("macins1", macins1);
+//         modelAndView.addObject("macins2", macins2);
+//         modelAndView.addObject("summa", macins1.plus(macins2));
+//         modelAndView.addObject("name", name);
+//         modelAndView.addObject("age", age);
+//         modelAndView.addObject("lessThan", macins3.lessThan(macins1));
+
+//         return modelAndView;
+//     }
+@GetMapping(value = "/test")
+@ResponseBody
+public String testAction() {
+    A a = new A();
+    return a.a();
+}
 }
